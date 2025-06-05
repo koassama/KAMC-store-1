5bb30a1ec0ded392b21ca00523d3383a49df8934
Radi1n <zxqi3@icloud.com>
2025-06-05 21:12:44 +0300

    Update consumers.php

--- admin/consumers.php
+++ admin/consumers.php
@@ -330,7 +330,7 @@ if (isset($_SESSION['admin'])) {
                                         <td>
                                          <ul class="list-group list-group-horizontal">
   <li class="list-group-item p-0 border-0">
-    <a href="reports.php?page=edit&id=<?= $row['id'] ?>" 
+    <a href="edit.php?id=<?php echo $post['id']; ?>"
        class="btn btn-warning btn-sm text-white mx-1" 
        style="width: 100px; height: 35px; border-radius: 8px;">
       <i class="fas fa-edit text-white"></i> تعديل
@@ -338,7 +338,7 @@ if (isset($_SESSION['admin'])) {
   </li>
 
   <li class="list-group-item p-0 border-0">
-    <a href="reports.php?page=delete&id=<?= $row['id'] ?>" 
+    <a href="delete.php?id=<?php echo $post['id']; ?>"  
        class="btn btn-danger btn-sm text-white mx-1" 
        style="width: 100px; height: 35px; border-radius: 8px;"
        onclick="return confirm('هل أنت متأكد من الحذف؟')">
